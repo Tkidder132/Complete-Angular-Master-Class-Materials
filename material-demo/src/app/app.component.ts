fb81f08cbdb656ea0e2bb460f7099c66511df8db
tkidder <no.name@google.com>
2019-08-16 19:53:25 -0500

    mat checkboxes

--- material-demo/src/app/app.component.ts
+++ material-demo/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isChecked = true;
   title = 'material-demo';
+
+  onChange($event) {
+    console.log($event);
+  }
 }
